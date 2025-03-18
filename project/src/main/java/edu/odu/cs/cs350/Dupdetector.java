a36f321871a9984730eeda1528bdd3c3161cf7cb
Raphael Afrim <rafri001@odu.edu>
2025-03-18 11:10:19 -0400

    fixed the file folder structure

---
+++ project/src/main/java/edu/odu/cs/cs350/Dupdetector.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350;
+
+
+public class Dupdetector {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ project/src/test/java/edu/odu/cs/cs350/DupdetectorTest.java
@@ -0,0 +1,12 @@
+package edu.odu.cs.cs350; 
+
+import org.junit.Test;
+
+
+public class DupdetectorTest {
+    
+    @Test  
+    public final void testMain() {
+        Dupdetector.main(new String[] {"arg1", "arg2"}); 
+    }
+}
